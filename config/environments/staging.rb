9e6dbcc1608eebc806e746f646eedbca7122c3f9
Corey Innis <corey@coolerator.net>
2008-11-12 23:34:41 -0800

    restarting with merb 1.0 (part 1)

--- config/environments/staging.rb
+++
@@ -1,7 +0,0 @@
-Merb.logger.info("Loaded STAGING Environment...")
-Merb::Config.use { |c|
-  c[:exception_details] = false
-  c[:reload_classes] = false
-  c[:log_level] = :error
-  c[:log_file] = Merb.log_path + "/staging.log"
-}
