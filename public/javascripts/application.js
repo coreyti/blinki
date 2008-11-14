999131678315a9ebe7cb270518eca102cad8ff88
Corey Innis <corey@coolerator.net>
2008-11-13 21:01:34 -0800

    playing around with markdown

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,6 @@
 // Common JavaScript code across your application goes here.
+$(document).ready(function() {
+  var container = $('pre#content.markdown');
+  var converted = new Showdown.converter().makeHtml(container.html());
+  container.replaceWith("<div>" + converted + "</div>");
+});
